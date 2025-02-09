d45916963d0bd3db7eefed2fad3cdee9427827ec
qe201020335 <qe201020335@sina.com>
2025-02-09 05:25:56 -0500

    Generate playlist cover with name

--- BeatSpiderSharp.Core/PlaylistExporter.cs
+++ BeatSpiderSharp.Core/PlaylistExporter.cs
@@ -5,6 +5,11 @@ using BeatSpiderSharp.Core.Utilities.Extensions;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Serilog;
+using SixLabors.Fonts;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Processing;
 
 namespace BeatSpiderSharp.Core;
 
@@ -68,15 +73,55 @@ public class PlaylistExporter
         serializer.Serialize(obj, targetPath);
     }
 
-    private Stream GetCover(string name)
+    private static Stream GetCover(string name)
     {
         var assembly = Assembly.GetExecutingAssembly();
-        var stream = assembly.GetManifestResourceStream("BeatSpiderSharp.Core.Assets.cover.png");
-        if (stream == null)
+        var coverStream = assembly.GetManifestResourceStream("BeatSpiderSharp.Core.Assets.cover.png");
+        using var fontStream = assembly.GetManifestResourceStream("BeatSpiderSharp.Core.Assets.font.ttf");
+        if (coverStream == null || fontStream == null)
         {
-            throw new NullReferenceException("Could not find cover.png in resources");
+            throw new NullReferenceException("Could not find cover.png or font.ttf in resources");
         }
 
-        return stream;
+        // Unlikely to happen, but just in case
+        if (string.IsNullOrWhiteSpace(name)) return coverStream;
+
+        // Load image
+        using var image = Image.Load(coverStream);
+        coverStream.Dispose();
+        
+        // Load font
+        FontCollection collection = new();
+        var family = collection.Add(fontStream);
+        Log.Debug("Using font {Name} ", family.Name);
+        var font = family.CreateFont(72, FontStyle.Regular);
+        
+        RichTextOptions options = new(font)
+        {
+            Origin = new PointF(image.Width / 2f, image.Height / 2f),
+            HorizontalAlignment = HorizontalAlignment.Center,
+            VerticalAlignment = VerticalAlignment.Center
+        };
+
+        // Scale font to fit image
+        var rect = TextMeasurer.MeasureSize(name, options);
+        var scalingFactor = image.Width * 0.8f / rect.Width;
+        var scaledFont = new Font(font, scalingFactor * font.Size);
+        options.Font = scaledFont;
+
+        // Draw text
+        image.Mutate(ctx =>
+        {
+            ctx.DrawText(options, name, Color.White);
+        });
+        
+        // Save image
+        var encoder = new JpegEncoder { Quality = 100 };
+#if DEBUG
+        image.SaveAsJpeg("cover-generated.jpg", encoder);
+#endif
+        var buffer = new MemoryStream();
+        image.SaveAsJpeg(buffer, encoder);
+        return buffer;
     }
 }
