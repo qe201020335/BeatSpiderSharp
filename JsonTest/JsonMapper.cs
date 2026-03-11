be581d4b8cee538b8a2716cf414f0d8cd19457f3
qe201020335 <qe201020335@sina.com>
2026-03-11 02:09:53 -0400

    Usable json testing proj

---
+++ JsonTest/JsonMapper.cs
@@ -0,0 +1,108 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace JsonTest;
+
+public static class JsonMapper
+{
+    private static JObject MapJObj(JObject jObj)
+    {
+        var map = new JObject();
+
+        foreach (var property in jObj.Properties())
+        {
+            map.Add(property.Name, MapJToken(property.Value));
+        }
+
+        return map;
+    }
+
+    private static JArray MapJArray(JArray arr)
+    {
+        var result = new JArray();
+        if (arr.Count == 0)
+        {
+            return result;
+        }
+    
+        // Assume all elements in the array are of the same type
+        result.Add(MapJToken(arr[0]));
+        return result;
+    }
+
+    public static JToken MapJToken(JToken? token)
+    {
+        // Console.WriteLine($"{token.Path}: {token.Type}");
+        token ??= JValue.CreateNull();
+
+        return token.Type switch
+        {
+            JTokenType.Array => MapJArray((token as JArray)!),
+            JTokenType.Object => MapJObj((token as JObject)!),
+            _ => token.Type.ToString()
+        };
+    }
+
+    private static JObject MergeJObj(JObject obj1, JObject? obj2)
+    {
+        var map = new JObject();
+
+        var propertyNames = obj1.Properties().Select(p => p.Name);
+        var names2 = obj2?.Properties().Select(p => p.Name);
+        if (names2 is not null)
+        {
+            propertyNames = propertyNames.Concat(names2).Distinct();
+        }
+
+        foreach (var propertyName in propertyNames)
+        {
+            var prop1 = obj1[propertyName];
+            var prop2 = obj2?[propertyName];
+
+            var result = MergeJToken(prop1, prop2);
+
+            map.Add(propertyName, result);
+        }
+
+        return map;
+    }
+
+    private static JArray MergeJArray(JArray arr1, JArray? arr2)
+    {
+        //reduce all elements to one
+        var reduced = arr1.Concat(arr2 ?? []).Aggregate<JToken, JToken?>(null, MergeJToken) ?? JValue.CreateNull();
+        return [reduced];
+    }
+
+    public static JToken MergeJToken(JToken? tok1, JToken? tok2)
+    {
+        tok1 ??= JValue.CreateNull();
+        tok2 ??= JValue.CreateNull();
+        if (tok1.Type == JTokenType.Null && tok2.Type == JTokenType.Null)
+        {
+            return JValue.CreateNull();
+        }
+
+        if (tok1.Type != tok2.Type && tok1.Type != JTokenType.Null && tok2.Type != JTokenType.Null)
+        {
+            throw new ArgumentException(
+                $"JToken type mismatch. {nameof(tok1)}: {tok1.Type}, {nameof(tok2)}: {tok2.Type})");
+        }
+
+        if (tok1.Type == JTokenType.Null)
+        {
+            (tok1, tok2) = (tok2, tok1);
+        }
+
+        if (tok2.Type == JTokenType.Null)
+        {
+            tok2 = null;
+        }
+
+        return tok1.Type switch
+        {
+            JTokenType.Array => MergeJArray((tok1 as JArray)!, (JArray?)tok2),
+            JTokenType.Object => MergeJObj((tok1 as JObject)!, (JObject?)tok2),
+            _ => tok1
+        };
+    }
+}
---
+++ JsonTest/Program.cs
@@ -0,0 +1,101 @@
+﻿using System.Diagnostics;
+using System.IO.Compression;
+using BeatSpiderSharp.Extensions;
+using BeatSpiderSharp.Models.BeatSaver;
+using JsonTest;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+Console.WriteLine("Hello, World!");
+if (args.Length == 0)
+{
+    Console.WriteLine("Missing json.gz path argument");
+    Environment.Exit(1);
+}
+
+var path = args[0];
+if (!File.Exists(path))
+{
+    Console.WriteLine($"File {path} does not exist");
+    Environment.Exit(1);
+}
+var jsonReader = new JsonTextReader(new StreamReader(new GZipStream(File.OpenRead(path), CompressionMode.Decompress)));
+var serializer = JsonSerializer.Create(new JsonSerializerSettings
+{
+    Formatting = Formatting.Indented,
+    // Uncomment this to check whether there are new fields in the json that are not in the model
+    // MissingMemberHandling = MissingMemberHandling.Error
+});
+
+
+var watch = Stopwatch.StartNew();
+
+var memoryPeak = GC.GetTotalMemory(false);
+var i = 0;
+var characteristics = new HashSet<string>();
+var difficulties = new HashSet<string>();
+var (reduced, sum) = await serializer
+    .DeserializeArrayAsync<JObject>(jsonReader, ["docs"])
+    .AggregateAsync(((JToken?)null, 0d), (acc, obj) =>
+{
+    if (obj is null) return acc;
+    var (reduced, sum) = acc;
+    reduced = JsonMapper.MergeJToken(obj, reduced);
+    try
+    {
+        var song = obj.ToObject<Song>(serializer)!;
+        foreach (var level in song.LatestVersion.Diffs)
+        {
+            if (!string.IsNullOrWhiteSpace(level.Characteristic))
+            {
+                characteristics.Add(level.Characteristic);
+            }
+
+            if (!string.IsNullOrWhiteSpace(level.Difficulty))
+            {
+                difficulties.Add(level.Difficulty);
+            }
+        }
+
+        if (song.Stats?.Plays != 0)
+        {
+            Console.WriteLine($"{song.Id} Plays: {song.Stats!.Plays}");
+        }
+        
+        sum += song.Id?.Length ?? 0;
+        sum += song.Metadata?.SongName?.Length ?? 0;
+        sum += song.Metadata?.SongAuthorName?.Length ?? 0;
+        sum += song.Metadata?.LevelAuthorName?.Length ?? 0;
+
+        var latest = song.Versions[0];
+        sum += latest.Hash?.Length ?? 0;
+
+        var diff = latest.Diffs[0];
+        sum += diff.Difficulty?.Length ?? 0;
+        sum += diff.Characteristic?.Length ?? 0;
+        sum += diff.Stars;
+        sum += diff.BlStars;
+        sum += diff.Notes;
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+        Console.WriteLine(obj.ToString());
+        Environment.Exit(1);
+    }
+    i++;
+    memoryPeak = Math.Max(memoryPeak, GC.GetTotalMemory(false));
+    return (reduced, sum);
+});
+
+watch.Stop();
+Console.WriteLine(
+    $"Time taken to merge json and query 10 values each from {i} songs: {watch.ElapsedMilliseconds / 1000f} s");
+Console.WriteLine($"Sum: {sum}");
+
+Console.WriteLine($"Peak memory usage: {memoryPeak / 1024 / 1024} MiB");
+Console.WriteLine($"Unique characteristics: {string.Join(", ", characteristics)}");
+Console.WriteLine($"Unique difficulties: {string.Join(", ", difficulties)}");
+var objMap = JsonMapper.MapJToken(reduced);
+Console.WriteLine("Json map:");
+Console.WriteLine(objMap);
